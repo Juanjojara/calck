717fd0b346c4ce3521579ab2ec8ab32124c3f751
j.jaralaconich <J.JaraLaconich@almaviva.it>
2021-09-07 23:41:23 +0200

    fixed test

--- calc.test.js
+++ calc.test.js
@@ -1,5 +1,5 @@
 const sum = require('./index');
 
-test('String calculator', (numbers) => {
-  expect(numbers.length === 0).toBe(0);
+test('String calculator', () => {
+  expect(add("")).toBe(0);
 });
