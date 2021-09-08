e6889abf5a513fc443d1609d4731b23535f4c6fe
j.jaralaconich <J.JaraLaconich@almaviva.it>
2021-09-08 23:37:42 +0200

    Added test for more than 2 numbers

--- calc.test.js
+++ calc.test.js
@@ -1,7 +1,11 @@
 const add = require('./calc');
 
-test('String calculator', () => {
+test('Limited string calculator', () => {
   expect(add("")).toBe(0);
   expect(add("10")).toBe(10);
   expect(add("10,3")).toBe(13);
 });
+
+test('Unlimited string calculator', () => {
+    expect(add("5,3,6,8,11")).toBe(33);
+  });
